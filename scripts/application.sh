9162f1e159a633ba657487129214b81e779ea20d
Gizem Gur <grgizem@gmail.com>
2018-02-05 09:43:12 +0300

    Changed tunneling text

--- scripts/application.sh
+++ scripts/application.sh
@@ -2,7 +2,8 @@
 
 APP_SERVICE=$(vagrant ssh k8s0 -c "kubectl --kubeconfig=/home/vagrant/admin.conf get services | grep hello | awk '{print \$3}'")
 
-echo "You can access to application from:"
-echo "http://$APP_SERVICE:80/"
-
-ssh -L 80:$APP_SERVICE:80 vagrant@127.0.0.1 -i .vagrant/machines/k8s1/virtualbox/private_key -p `vagrant port --guest 22 k8s1`
+echo "You can access to application with ssh tunneling:"
+echo "Application IP: $APP_SERVICE"
+echo "Vagrant Port: $(vagrant port --guest 22 k8s1)"
+echo "Use following command, then access from your local browser:" 
+echo "ssh -L 80:<Application IP>:80 vagrant@127.0.0.1 -i .vagrant/machines/k8s1/virtualbox/private_key -p <Vagrant Port>"
